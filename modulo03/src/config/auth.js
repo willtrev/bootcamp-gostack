16aad07db2929561bb6d1d24b2ed09e6198e9d30
willtrev <williantrev@gmail.com>
2020-02-11 11:01:20 -0300

    Modulo 03

---
+++ modulo03/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'f51fee88b3770cd82287e42560b2f782',
+  expiresIn: '7d',
+};
---
+++ modulo03/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '9cacfa6d0f187b',
+    pass: '41ddfebf5757c5',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
